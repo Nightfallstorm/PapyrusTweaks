f4833dc066240f68fd22da0294c09f6b311f163e
nightfallstorm <109926124+Nightfallstorm@users.noreply.github.com>
2022-11-01 21:07:47 -0500

    Convert back to po3 fork for dev work

--- src/PCH.h
+++ src/PCH.h
@@ -56,6 +56,42 @@ namespace stl
 	}
 }
 
+namespace REL
+{
+
+	#define Module tempModule
+
+	class tempModule
+	{
+	public:
+#ifdef SKYRIM_VR
+		static bool IsVR(){ return true; }
+		static bool IsAE() { return false; }
+		static bool IsSE() { return false; }
+#elif SKYRIM_AE
+		static bool IsVR(){ return false; }
+		static bool IsAE() { return true; }
+		static bool IsSE() { return false; }
+#else
+		static bool IsVR(){ return false; }
+		static bool IsAE() { return false; }
+		static bool IsSE() { return true; }
+#endif
+	};
+
+	static int VariantOffset(int SE, int AE, int VR)
+	{
+#ifdef SKYRIM_VR
+		return VR;
+
+#elif SKYRIM_AE
+		return AE;
+#else
+		return SE;
+#endif
+	}
+}
+
 #define DLLEXPORT __declspec(dllexport)
 
 #include "Version.h"
